3c7a69762da23f4b01a67cc571915e5d8b62f87a
MD ARAFAT ANOWAR <arafat-anowar@outlook.com>
2026-07-18 00:13:07 +0600

    Revert "Implement Admin Dashboard and Improve Project Structure"

--- includes/admin.h
+++ includes/admin.h
@@ -1,20 +1,7 @@
 #ifndef ADMIN_H
 #define ADMIN_H
 
-#define input_capture _getche()
-#define clear_screen system("cls")
-#define ESC 27
 void admin_dashboard_screen();
 void admin_dashboard(char user_id[]);
 void welcome_screen();
-void admin_menu();
-void main_menu();
-char admin_dashboard_redirect_menu(choice);
-void welcome_screen();
-void manage_doctor();
-void manage_receptionist();
-void manage_patient();
-void manage_appointments();
-void manage_bills();
-void search_user_by_id();
 #endif
--- includes/login.h
+++ includes/login.h
@@ -1,37 +1,11 @@
-/*
-Group - 4
-Members :
-    FAIZA LABIBA
-    ID : 2026-2-60-013
-    MD ARAFAT ANOWAR
-    ID : 2026-2-60-020
-    KAMRUL HASAN RIFAT
-    ID : 2026-2-60-022
-*/
-// Header File Guard
 #ifndef LOGIN_H
 #define LOGIN_H
-// Global Header Files
-#include <stdio.h>
-#include <windows.h>
-#include <unistd.h>
-// Local Header Files
-#include "admin.h"
-#include "receptionist.h"
-#include "doctor.h"
-#include "patient.h"
-#include "bloodbank.h"
-// Macros
-#define clear_screen system("cls")
-#define input_capture _getche()
-#define ESC 27
-// Declare Funtions Prototype Which Are In login.c
+
 void login();
 void login_screen();
 void login_success_screen();
 void login_invaild_screen();
-char login_verify(char user_name[], char user_password[]);
+char login_verify(char user_name[],char user_password[]);
 void redirect_to_dashboard(char role);
-// Functions From Another Header File
-// void welcome_screen();
+void welcome_screen();
 #endif
