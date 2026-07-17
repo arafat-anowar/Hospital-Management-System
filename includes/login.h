09bcb0c27be72e8e9d90826c5e4d3b7eda5e5be2
arafat-anowar <arafat-anowar@outlook.com>
2026-07-18 05:36:00 +0600

    Fixed Issue By revert code

--- includes/login.h
+++ includes/login.h
@@ -1,3 +1,14 @@
+/*
+Group - 4
+Members :
+    FAIZA LABIBA
+    ID : 2026-2-60-013
+    MD ARAFAT ANOWAR
+    ID : 2026-2-60-020
+    KAMRUL HASAN RIFAT
+    ID : 2026-2-60-022
+*/
+// Header File Guard
 #ifndef LOGIN_H
 #define LOGIN_H
 // Global Header Files
@@ -13,6 +24,7 @@
 // Macros
 #define clear_screen system("cls")
 #define input_capture _getche()
+#define screen_delay sleep(1)
 #define ESC 27
 // Declare Funtions Prototype Which Are In login.c
 void login();
@@ -21,6 +33,4 @@ void login_success_screen();
 void login_invaild_screen();
 char login_verify(char user_name[],char user_password[]);
 void redirect_to_dashboard(char role);
-// Functions From Another Header File
-// void welcome_screen();
 #endif
