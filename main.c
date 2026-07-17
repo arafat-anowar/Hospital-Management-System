90fa216ebb31b1c8a2f88e3ba98f68f22ace6a80
arafat-anowar <arafat-anowar@outlook.com>
2026-07-17 17:49:58 +0600

    Improve Menu

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <conio.h>
 #include <windows.h>
 #include <time.h>
+#include <dos.h>
 
 // Local Header File
 #include "includes\login.h"
@@ -19,7 +20,7 @@ void main_menu();
 char navigate_to_menu(char choice);
 // Macros
 #define clear_screen system("cls")
-#define input_capture _getch()
+#define input_capture _getche()
 int main()
 {
     main_menu();
@@ -44,19 +45,23 @@ char navigate_to_menu(char choice)
         break;
     }
 }
+// void show_date_time() {
+//
+//     printf("+ ============================================================= +\n");
+//     printf("| Date  : Time :|");
+//     printf("+ ============================================================= +\n");
+// }
 void main_menu()
 {
     clear_screen;
     welcome_screen();
     // This Shows Main Menu
-    printf("+---------------------------------------------------------------+\n");
-    printf("| Enter Your Choice:                                            |\n");
-    printf("| 1 -> Book an Appointment                                      |\n");
-    printf("| 2 -> Login                                                    |\n");
-    printf("| 3 -> Blood Bank Information                                   |\n");
-    printf("| 4 -> Chatbot                                                  |\n");
-    printf("+---------------------------------------------------------------+\n");
-    printf("\n");
+    printf("+------------------------[ MAIN MENU ]--------------------------+\n");
+    printf("| [1] -> Book an Appointment                                    |\n");
+    printf("| [2] -> Login                                                  |\n");
+    printf("| [3] -> Blood Bank Information                                 |\n");
+    printf("| [4] -> Chatbot                                                |\n");
+    printf("+---------------------[PRESS YOUR CHOICE]-----------------------+\n");
     // Take Input from user and check the input is between 1 to 4
     char choice;
     do
